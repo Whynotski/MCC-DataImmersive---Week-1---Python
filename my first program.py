b69f73a56578f84cc57aa8a9226682b43f5be08a
Whynotski <laurawynosky@outlook.com>
2019-04-08 14:54:33 -0400

    Day 1 exercises

---
+++ my first program.py
@@ -0,0 +1,25 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:54:40) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> print ("hello world")
+hello world
+>>> x =5
+>>> print X
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(X)?
+>>> print x
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(x)?
+>>> print (x)
+5
+>>> r = "hello"
+>>> s = "world"
+>>> r+s
+'helloworld'
+>>> 'helloworld'
+'helloworld'
+>>> print (r + ' ' + s)
+hello world
+>>> 
+================== RESTART: C:/Python36/my.first.program.py ==================
+This test worked.
+>>> 
